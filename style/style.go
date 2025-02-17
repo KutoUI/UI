a9b7bc625bfe04a6f49f8ab8ba31785f66e59e91
wsor4035 <24964441+wsor4035@users.noreply.github.com>
2025-02-17 07:18:23 +0000

    add style start and license

---
+++ style/style.go
@@ -0,0 +1,18 @@
+package style
+
+import (
+    "embed"
+)
+
+//go:embed css/*.css
+var folder embed.FS
+
+func GetSharedStyle() string {
+
+	reset, _ := folder.ReadFile("css/reset.css")
+	boxmodel, _ := folder.ReadFile("css/boxmodel.css")
+	colors, _ := folder.ReadFile("css/colors.css")
+	core, _ := folder.ReadFile("css/core.css")
+
+	return string(reset)+string(boxmodel)+string(colors)+string(core)
+}
