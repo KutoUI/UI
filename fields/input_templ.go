d5c3accebe988a10b630e3e484c5eb2a09416261
wsor4035 <24964441+wsor4035@users.noreply.github.com>
2025-03-08 00:06:21 +0000

    style file input selector better

--- fields/input_templ.go
+++ fields/input_templ.go
@@ -49,7 +49,7 @@ func inputStyle() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<style type=\"text/css\">\n        .kutoui_input {\n\n            label {\n                display: block;\n                padding-bottom: 0.5rem;\n            }\n\n            & > div {\n                border: 1px solid var(--border-color);\n                border-radius: 0.5rem;\n                display: flex;\n                gap: 1rem;\n                align-items: center;\n                padding: 0 0.5rem;\n\n                &:has(:focus-visible) {\n                    outline-offset: 0px;\n                    outline: -webkit-focus-ring-color auto 1px;\n                }\n            \n                input {\n                    display: block;\n                    outline: 0px transparent;\n                    border: 0px transparent;\n                    color: inherit;\n                    background-color: transparent;\n                    width: 100%;\n                    padding: 0.5rem 0;\n\n                    @media (prefers-color-scheme: dark) {\n                        &::-webkit-calendar-picker-indicator {\n                            filter: invert(0.8);\n                        }\n                    }\n                }\n            }\n\n        }\n    </style>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<style type=\"text/css\">\n        .kutoui_input {\n\n            label {\n                display: block;\n                padding-bottom: 0.5rem;\n            }\n\n            & > div {\n                border: 1px solid var(--border-color);\n                border-radius: 0.5rem;\n                display: flex;\n                gap: 1rem;\n                align-items: center;\n                padding: 0 0.5rem;\n\n                &:has(:focus-visible) {\n                    outline-offset: 0px;\n                    outline: -webkit-focus-ring-color auto 1px;\n                }\n            \n                input {\n                    display: block;\n                    outline: 0px transparent;\n                    border: 0px transparent;\n                    color: inherit;\n                    background-color: transparent;\n                    width: 100%;\n                    padding: 0.5rem 0;\n\n                    &::file-selector-button {\n                        background-color: transparent;\n                        color: inherit;\n                        border: 0px transparent;\n                        line-height: inherit;\n                        font-weight: bold;\n                    }\n\n                    @media (prefers-color-scheme: dark) {\n                        &::-webkit-calendar-picker-indicator {\n                            filter: invert(0.8);\n                        }\n                    }\n                }\n            }\n\n        }\n    </style>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -132,7 +132,7 @@ func inputDisplay(i Input) templ.Component {
 			var templ_7745c5c3_Var6 string
 			templ_7745c5c3_Var6, templ_7745c5c3_Err = templ.JoinStringErrs(i.BaseField.Id)
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `local-libs/ui/fields/input.templ`, Line: 76, Col: 38}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `local-libs/ui/fields/input.templ`, Line: 84, Col: 38}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var6))
 			if templ_7745c5c3_Err != nil {
@@ -145,7 +145,7 @@ func inputDisplay(i Input) templ.Component {
 			var templ_7745c5c3_Var7 string
 			templ_7745c5c3_Var7, templ_7745c5c3_Err = templ.JoinStringErrs(i.BaseField.Label)
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `local-libs/ui/fields/input.templ`, Line: 76, Col: 58}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `local-libs/ui/fields/input.templ`, Line: 84, Col: 58}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var7))
 			if templ_7745c5c3_Err != nil {
@@ -178,7 +178,7 @@ func inputDisplay(i Input) templ.Component {
 			var templ_7745c5c3_Var8 string
 			templ_7745c5c3_Var8, templ_7745c5c3_Err = templ.JoinStringErrs(i.Type)
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `local-libs/ui/fields/input.templ`, Line: 85, Col: 32}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `local-libs/ui/fields/input.templ`, Line: 93, Col: 32}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var8))
 			if templ_7745c5c3_Err != nil {
@@ -196,7 +196,7 @@ func inputDisplay(i Input) templ.Component {
 		var templ_7745c5c3_Var9 string
 		templ_7745c5c3_Var9, templ_7745c5c3_Err = templ.JoinStringErrs(i.BaseField.Name)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `local-libs/ui/fields/input.templ`, Line: 87, Col: 38}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `local-libs/ui/fields/input.templ`, Line: 95, Col: 38}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var9))
 		if templ_7745c5c3_Err != nil {
@@ -209,7 +209,7 @@ func inputDisplay(i Input) templ.Component {
 		var templ_7745c5c3_Var10 string
 		templ_7745c5c3_Var10, templ_7745c5c3_Err = templ.JoinStringErrs(i.BaseField.Id)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `local-libs/ui/fields/input.templ`, Line: 88, Col: 34}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `local-libs/ui/fields/input.templ`, Line: 96, Col: 34}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var10))
 		if templ_7745c5c3_Err != nil {
@@ -232,7 +232,7 @@ func inputDisplay(i Input) templ.Component {
 		var templ_7745c5c3_Var11 string
 		templ_7745c5c3_Var11, templ_7745c5c3_Err = templ.JoinStringErrs(strconv.Itoa(i.BaseField.MinLength))
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `local-libs/ui/fields/input.templ`, Line: 90, Col: 62}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `local-libs/ui/fields/input.templ`, Line: 98, Col: 62}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var11))
 		if templ_7745c5c3_Err != nil {
@@ -245,7 +245,7 @@ func inputDisplay(i Input) templ.Component {
 		var templ_7745c5c3_Var12 string
 		templ_7745c5c3_Var12, templ_7745c5c3_Err = templ.JoinStringErrs(strconv.Itoa(i.BaseField.MaxLength))
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `local-libs/ui/fields/input.templ`, Line: 91, Col: 62}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `local-libs/ui/fields/input.templ`, Line: 99, Col: 62}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var12))
 		if templ_7745c5c3_Err != nil {
@@ -258,7 +258,7 @@ func inputDisplay(i Input) templ.Component {
 		var templ_7745c5c3_Var13 string
 		templ_7745c5c3_Var13, templ_7745c5c3_Err = templ.JoinStringErrs(i.PlaceHolder)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `local-libs/ui/fields/input.templ`, Line: 92, Col: 42}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `local-libs/ui/fields/input.templ`, Line: 100, Col: 42}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var13))
 		if templ_7745c5c3_Err != nil {
@@ -271,7 +271,7 @@ func inputDisplay(i Input) templ.Component {
 		var templ_7745c5c3_Var14 string
 		templ_7745c5c3_Var14, templ_7745c5c3_Err = templ.JoinStringErrs(i.BaseField.Value)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `local-libs/ui/fields/input.templ`, Line: 93, Col: 40}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `local-libs/ui/fields/input.templ`, Line: 101, Col: 40}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var14))
 		if templ_7745c5c3_Err != nil {
